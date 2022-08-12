ecffce43615e04b7c32f7b0ad5045702bc3c9829
saronbrandao <saronbrandao@gmail.com>
2022-08-12 15:05:13 +0200

    initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header>
+        <h1>ReactMeals</h1>
+      </header>
+      <div></div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
