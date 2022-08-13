7e3640ea1632e7d6693bc7f6cbe98a51091fd307
saronbrandao <saronbrandao@gmail.com>
2022-08-13 14:09:23 +0200

    version_1

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
-import classes from './Cart.modules.css';
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
 
 const Cart = (props) => {
   const cartItems = (
@@ -10,14 +11,17 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
-      <div className={classes.actions}></div>
-    </div>
+      <div className={classes.actions}>
+        <button className={classes['.button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
   );
 };
 
